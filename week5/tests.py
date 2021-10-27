819da3761630d2d4b54a8bf2b003882e23251cbf
Mateo Estrada <mateoej12@gmail.com>
2021-10-27 11:02:42 -0700

    Added new stuff for the class.

---
+++ week5/tests.py
@@ -0,0 +1,10 @@
+# Mateo Estrada Jorge
+# Software Engineering II
+# Random Testing
+# Goals:
+# 1. pratice writing random tests
+# 2. pratice trying to diagnose the cause of the discovered bugs
+
+from credit_card_validator import credit_card_validator
+
+
